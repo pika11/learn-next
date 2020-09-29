2173342cf8a18f2d58cea6cf9d61760c7582602b
Alejandro Foronda <aforonda90@gmail.com>
2020-09-28 23:09:19 -0400

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,60 @@
-import Nav from '../components/nav'
+import { BigHead } from "@bigheads/core"
 
-export default function IndexPage() {
-  return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
-        </h1>
-      </div>
-    </div>
-  )
+export default function Index() {
+	return (
+		<div>
+			<div className="">
+				<h1 className="">
+					Learn NEXT.JS by doing
+				</h1>
+				<p>
+					learn by doing is the best way
+					yo learn
+				</p>
+				<p>
+					"doing.... what!?", you ask?
+				</p>
+			</div>
+			<div className="">
+				<BigHead
+					accessory="none"
+					body="chest"
+					circleColor="blue"
+					clothing="shirt"
+					clothingColor="black"
+					eyebrows="concerned"
+					eyes="normal"
+					faceMask
+					faceMaskColor="black"
+					facialHair="none"
+					graphic="react"
+					hair="afro"
+					hairColor="orange"
+					hat="none3"
+					hatColor="red"
+					lashes={false}
+					lipColor="green"
+					mask
+					mouth="tongue"
+					skinTone="light"
+				/>
+				<p>hey! I need your help!</p>
+				<p>
+					I'm Karm Xubergerg, I need you
+					to creat a Next site for me
+					now!
+				</p>
+				<p>
+					It must have one page with the
+					title "ToeBook"
+				</p>
+				<p>
+					Toes will be the next big
+					thing!, Hurry!
+				</p>
+				<img src="/images/nextjs-logo.svg" />
+				<button>Start!</button>
+			</div>
+		</div>
+	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'accent-1': '#333',
-      },
-    },
-  },
-  variants: {},
-  plugins: [],
+	future: {
+		removeDeprecatedGapUtilities: true,
+	},
+	purge: [
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
 }
