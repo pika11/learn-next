08a0237ecb6d6122592e582d3e822a20e5f717fe
Alejandro Foronda <aforonda90@gmail.com>
2020-10-02 00:00:29 -0400

    Create components for svg path icons

---
+++ components/icons/arrow-right.js
@@ -0,0 +1,20 @@
+export default function ArrowRight({
+	className,
+}) {
+	return (
+		<svg
+			className={className}
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth={2}
+				d="M14 5l7 7m0 0l-7 7m7-7H3"
+			/>
+		</svg>
+	)
+}
