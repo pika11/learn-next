98087cbcc22ebf56861d844604e551fdbfb883db
Alejandro Foronda <aforonda90@gmail.com>
2020-10-04 02:00:10 -0400

    Set challenge 2 layout

--- pages/challenges/2.js
+++ pages/challenges/2.js
@@ -1,9 +1,22 @@
 import Layout from "../../components/layout"
+import Mark from "../../components/bigheads/mark"
+import NextLogo from "../../components/images/next-logo"
 
 export default function challenge2() {
 	return (
 		<>
-			<Layout>Hello</Layout>
+			<Layout>
+				<div className="w-24 mt-4">
+					<Mark />
+				</div>
+				<h1 className="text-lg pt-1 pb-2 font-bold">
+					Challenge 2:
+				</h1>
+				<p className="text-gray-300 italic pb-8 text-center font-bold">
+					Create an h1 tag that says ToeBook in
+					the main page
+				</p>
+			</Layout>
 		</>
 	)
 }
