4eb119596d07b07d5e8f754f1f920ec24123885f
Alejandro Foronda <aforonda90@gmail.com>
2020-10-05 20:18:09 -0400

    Update 2.js
    Update formating

--- pages/challenges/2.js
+++ pages/challenges/2.js
@@ -9,12 +9,9 @@ export default function challenge2() {
 				<div className="w-24 mt-4">
 					<Mark />
 				</div>
-				<h1 className="text-lg pt-1 pb-2 font-bold">
-					Challenge 2:
-				</h1>
-				<p className="text-gray-300 italic pb-8 text-center font-bold">
-					Create an h1 tag that says ToeBook in
-					the main page
+				<h1 className="text-lg pt-1 pb-2 font-bold">Challenge 2:</h1>
+				<p className="text-gray-300 italic pb-4 text-center font-bold">
+					Create an h1 tag that says ToeBook in the main page
 				</p>
 			</Layout>
 		</>
