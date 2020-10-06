254c8bbe43fd5785fe2467daca015a24cf392163
Alejandro Foronda <aforonda90@gmail.com>
2020-10-05 20:17:39 -0400

    Update layout.js
    Update formating

--- components/layout.js
+++ components/layout.js
@@ -2,9 +2,7 @@ export default function Layout({ children }) {
 	return (
 		<div className="flex justify-center">
 			<div className="max-w-md px-2 flex-1">
-				<div className="flex flex-col items-center">
-					{children}
-				</div>
+				<div className="flex flex-col items-center">{children}</div>
 			</div>
 		</div>
 	)
