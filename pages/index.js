4b6d28e27a81a04da6a55741da869a8512948130
Alejandro Foronda <aforonda90@gmail.com>
2020-10-05 20:17:51 -0400

    Update index.js
    Update formating

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,7 @@ export default function Index() {
 		},
 	}
 
-	const [
-		buttonVariant,
-		setButtonVariant,
-	] = useState("default")
+	const [buttonVariant, setButtonVariant] = useState("default")
 
 	const setDefault = () => {
 		setButtonVariant("default")
@@ -36,17 +33,14 @@ export default function Index() {
 
 	return (
 		<Layout>
-			<h1 className="text-3xl font-bold mt-8">
-				Learn{" "}
-				<NextLogo className="inline-block w-32 mx-2 my-1 text-white" />{" "}
-				by doing
+			<h1 className="text-3xl font-bold mt-8 text-center">
+				Learn <NextLogo className="inline-block w-32 mx-2 my-1 text-white" /> by
+				doing
 			</h1>
 			<p className="text-sm italic text-gray-200">
 				"learn by doing" is the best way to learn
 			</p>
-			<p className="font-mono text-lg mt-3">
-				"doing.... what!?", you ask?
-			</p>
+			<p className="font-mono text-lg mt-3">"doing.... what!?", you ask?</p>
 			<motion.div
 				className="w-3/4 pt-8 pb-1"
 				animate={{
@@ -64,23 +58,13 @@ export default function Index() {
 				"hey! I need your help!"
 			</p>
 			<p className="text-center my-6 text-gray-300">
-				I'm{" "}
-				<strong className="text-gray-100">
-					Karm Xubergerg
-				</strong>
-				, I need{" "}
-				<strong className="text-gray-100">
-					you
-				</strong>{" "}
-				to create a{" "}
-				<NextLogo className="inline-block w-16 mx-2" />{" "}
-				site for me now!
+				I'm <strong className="text-gray-100">Karm Xubergerg</strong>, I need{" "}
+				<strong className="text-gray-100">you</strong> to create a{" "}
+				<NextLogo className="inline-block w-16 mx-2" /> site for me now!
 			</p>
 			<p className="text-center mb-16 text-gray-300">
 				It must have one page with the title{" "}
-				<strong className="text-gray-100">
-					ToeBook
-				</strong>
+				<strong className="text-gray-100">ToeBook</strong>
 				<br />
 				Toes will be the next big thing!, Hurry!
 			</p>
@@ -93,9 +77,7 @@ export default function Index() {
 							border-pink-900"
 					variants={variants}
 					animate={buttonVariant}
-					onHoverStart={() =>
-						setButtonVariant("hover")
-					}
+					onHoverStart={() => setButtonVariant("hover")}
 					onTap={() => setButtonVariant("tap")}
 					onHoverEnd={setDefault}
 					onTapCancel={setDefault}
