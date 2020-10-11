37e5b00aae2e603a354746885a4c479ae44914b1
Alejandro Foronda <aforonda90@gmail.com>
2020-10-11 10:37:18 -0400

    Only render buttons on client side

--- components/footer.js
+++ components/footer.js
@@ -2,8 +2,7 @@ import { useEffect } from "react"
 
 export default function Footer() {
 	useEffect(() => {
-		if (typeof twttr !== "undefined") 
-			twttr.widgets.load()
+		if (typeof twttr !== "undefined") twttr.widgets.load()
 	}, [])
 
 	return (
@@ -11,22 +10,24 @@ export default function Footer() {
 			<p className="text-xs text-center text-gray-300">
 				suggestions, feedback and comments are greatly appreciated!
 			</p>
-			<div className="flex justify-around pt-2">
-				<a
-					href="https://twitter.com/PIKA11?ref_src=twsrc%5Etfw"
-					className="twitter-follow-button"
-					data-show-count="false"
-				>
-					Follow @PIKA11
-				</a>
-				<a
-					href="https://twitter.com/intent/tweet?screen_name=PIKA11&ref_src=twsrc%5Etfw"
-					className="twitter-mention-button"
-					data-show-count="false"
-				>
-					Tweet to @PIKA11
-				</a>
-			</div>
+			{typeof window !== "undefined" && (
+				<div className="flex justify-around pt-2">
+					<a
+						href="https://twitter.com/PIKA11?ref_src=twsrc%5Etfw"
+						className="twitter-follow-button"
+						data-show-count="false"
+					>
+						Follow @PIKA11
+					</a>
+					<a
+						href="https://twitter.com/intent/tweet?screen_name=PIKA11&ref_src=twsrc%5Etfw"
+						className="twitter-mention-button"
+						data-show-count="false"
+					>
+						Tweet to @PIKA11
+					</a>
+				</div>
+			)}
 		</div>
 	)
 }
