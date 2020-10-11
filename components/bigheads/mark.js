9568eab60f1a16193486c60f98fdf9cef6fa6f1a
Alejandro Foronda <aforonda90@gmail.com>
2020-10-11 12:05:13 -0400

    Update Mark's head make it clickable to go to bigheads.io

--- components/bigheads/mark.js
+++ components/bigheads/mark.js
@@ -1,27 +1,29 @@
 import { BigHead } from "@bigheads/core"
 export default function Mark() {
 	return (
-		<BigHead
-			accessory="none"
-			body="chest"
-			circleColor="blue"
-			clothing="shirt"
-			clothingColor="black"
-			eyebrows="concerned"
-			eyes="normal"
-			faceMask
-			faceMaskColor="black"
-			facialHair="none"
-			graphic="react"
-			hair="afro"
-			hairColor="orange"
-			hat="none3"
-			hatColor="red"
-			lashes={false}
-			lipColor="green"
-			mask
-			mouth="tongue"
-			skinTone="light"
-		/>
+		<a href="https://bigheads.io/" target="_blank">
+			<BigHead
+				accessory="none"
+				body="chest"
+				circleColor="blue"
+				clothing="shirt"
+				clothingColor="black"
+				eyebrows="concerned"
+				eyes="normal"
+				faceMask
+				faceMaskColor="black"
+				facialHair="none"
+				graphic="react"
+				hair="afro"
+				hairColor="orange"
+				hat="none3"
+				hatColor="red"
+				lashes={false}
+				lipColor="green"
+				mask
+				mouth="tongue"
+				skinTone="light"
+			/>
+		</a>
 	)
 }
