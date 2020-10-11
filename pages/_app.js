d0b9a9a1b7ffd27f36cd8c8f713dc064805a0fa6
Alejandro Foronda <aforonda90@gmail.com>
2020-10-10 22:36:26 -0400

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
+				<link rel="icon" href="/favicon.png" />
 				<script async src="https://platform.twitter.com/widgets.js"></script>
 			</Head>
 			<Component {...pageProps} />
