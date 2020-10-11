e6d95e9070d88e749e7bf7735ff35d20be3cc201
Alejandro Foronda <aforonda90@gmail.com>
2020-10-10 22:35:47 -0400

    Import twitter buttons

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
+				<script async src="https://platform.twitter.com/widgets.js"></script>
 			</Head>
 			<Component {...pageProps} />
 		</>
